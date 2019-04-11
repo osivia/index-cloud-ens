cae8c8724c44898274c46710946a24674e607ffe
loic <loic@bebop>
2019-04-11 17:47:31 +0200

    #1964 - create user workspace in specific path

--- index-cloud-ens-nuxeo/index-cloud-ens-nuxeo-custom/src/main/java/org/osivia/demo/userworkspaces/UserWorkspaceService.java
+++ index-cloud-ens-nuxeo/index-cloud-ens-nuxeo-custom/src/main/java/org/osivia/demo/userworkspaces/UserWorkspaceService.java
@@ -1,7 +1,12 @@
 package org.osivia.demo.userworkspaces;
 
+import java.io.Serializable;
 import java.security.Principal;
 
+import org.apache.commons.lang.Validate;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.common.utils.Path;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
@@ -21,6 +26,11 @@ public class UserWorkspaceService extends ToutaticeUserWorkspaceServiceImpl {
 	 * 
 	 */
 	private static final long serialVersionUID = 6465767707468908495L;
+
+	/**
+	 * 
+	 */
+	private static final String TTC_TEMPLATE = "ttc:pageTemplate";
 	/**
 	 * 
 	 */
@@ -28,60 +38,21 @@ public class UserWorkspaceService extends ToutaticeUserWorkspaceServiceImpl {
 	/**
 	 * 
 	 */
+	private static final String TTC_WEBID = "ttc:webid";
+	/**
+	 * 
+	 */
 	private static final String DC_TITLE = "dc:title";
+	
+	
 
-	@Override
-	protected DocumentModel doCreateUserWorkspace(
-			CoreSession unrestrictedSession, PathRef wsRef,
-			Principal principal, String userName) throws ClientException {
-		DocumentModel userWorkspace = super.doCreateUserWorkspace(
-				unrestrictedSession, wsRef, principal, userName);
+    private static final Log log = LogFactory.getLog(UserWorkspaceService.class);
 
-		// Paramétrage affichage portail
-		// userWorkspace.setProperty("toutatice", "pageTemplate",
-		// "/templates/userWorkspace");
-		userWorkspace.setProperty("toutatice", "tabOrder", "100");
-		userWorkspace.setPropertyValue(DC_TITLE, "Mon espace");
 
-		unrestrictedSession.saveDocument(userWorkspace);
+    /** Segments prefix size of UserWorkspace. */
+    protected static final int UW_PREFIX_SEGMENT_SIZE = 3;
 
-		// Initialisation Mes documents
-		DocumentModel mesDocs = unrestrictedSession.createDocumentModel(
-				userWorkspace.getPathAsString(), "documents", "Folder");
-		mesDocs.setPropertyValue(DC_TITLE, "Documents");
-		mesDocs.setPropertyValue(TTC_SHOW_IN_MENU, Boolean.TRUE);
-
-		unrestrictedSession.createDocument(mesDocs);
-
-		// Initialisation Mes photos
-		DocumentModel mesPhotos = unrestrictedSession.createDocumentModel(
-				userWorkspace.getPathAsString(), "gallery",
-				"PictureBook");
-		mesPhotos.setPropertyValue(DC_TITLE, "Galerie d'images");
-		mesPhotos.setPropertyValue(TTC_SHOW_IN_MENU, Boolean.TRUE);
-
-		unrestrictedSession.createDocument(mesPhotos);
-
-		// Initialisation Mon agenda
-		DocumentModel monAgenda = unrestrictedSession.createDocumentModel(
-				userWorkspace.getPathAsString(), "calendar", "Agenda");
-		monAgenda.setPropertyValue(DC_TITLE, "Agenda");
-		monAgenda.setPropertyValue(TTC_SHOW_IN_MENU, Boolean.TRUE);
-
-		unrestrictedSession.createDocument(monAgenda);
-
-		// Initialisation du dossier public
-		DocumentModel publicFolder = unrestrictedSession.createDocumentModel(
-				userWorkspace.getPathAsString(), "public", "Folder");
-		publicFolder.setPropertyValue(DC_TITLE, "Documents partagés");
-		publicFolder.setPropertyValue(TTC_SHOW_IN_MENU, Boolean.TRUE);
-
-		unrestrictedSession.createDocument(publicFolder);
-		setFoldersACL(publicFolder, userName);
-
-		return userWorkspace;
-	}
-
+	
 	protected void setFoldersACL(DocumentModel doc, String userName)
 			throws ClientException {
 
@@ -96,4 +67,127 @@ public class UserWorkspaceService extends ToutaticeUserWorkspaceServiceImpl {
 		acp.addACL(acl);
 		doc.setACP(acp, true);
 	}
+
+    /**
+     * Gets UserWorkspace pathRef checking old policy first.
+     * <ul>
+     * <li>Actual UserWorkspace policy: /home/u/s/e/userlambda</li>
+     * <li>Old UserWorkspace policy : /userworkspace-domain/userworkspaces/u/s/e/userlambda</li>
+     * </ul>
+     */
+    @Override
+    protected PathRef resolveUserWorkspace(CoreSession session, PathRef homeDomainRef, String username, String workspacename, int maxsize) {
+        // UserWs path
+        PathRef usWsRef = null;
+        // Last segment
+        String usWsName = super.getUserWorkspaceNameForUser(username);
+
+    	Path usWsPath = computeUserWorspacePath(homeDomainRef.toString(), usWsName);
+        // Ref
+        usWsRef = new PathRef(usWsPath.toString());
+
+        return usWsRef;
+    }
+
+    /**
+     * Computes full UserWorkspace path.
+     * 
+     * @param rootPath
+     * @param usWsName
+     * @return full UserWorkspace Path
+     */
+    protected Path computeUserWorspacePath(String rootPath, String usWsName) {
+        Path usWsPath = new Path("/" + rootPath);
+
+        for (int i = 0; i < usWsName.length() && i < UW_PREFIX_SEGMENT_SIZE; i++) {
+            usWsPath = usWsPath.append(usWsName.charAt(i) + "/");
+        }
+        usWsPath = usWsPath.append(usWsName);
+
+        return usWsPath;
+    }
+
+
+    @Override
+    protected DocumentModel doCreateUserWorkspace(CoreSession unrestrictedSession, PathRef userWSRef, Principal principal, String userName)
+            throws ClientException {
+        // User Workspace
+        DocumentModel uw = null;
+        Path uwsPath = new Path(userWSRef.toString());
+
+        for (int i = UW_PREFIX_SEGMENT_SIZE - 1; i < uwsPath.segmentCount(); i++) {
+
+            if (log.isDebugEnabled()) {
+                log.debug("    - Création du document " + uwsPath.uptoSegment(i).toString());
+            }
+
+            PathRef nodeRef = new PathRef(uwsPath.uptoSegment(i).toString());
+            if (!unrestrictedSession.exists(nodeRef)) {
+                DocumentModel node = createUserWorkspacesRootSegment(unrestrictedSession, nodeRef);
+                Validate.isTrue(node.getPathAsString().equals(nodeRef.toString()));
+            }
+        }
+        // create user WS if needed
+        if (!unrestrictedSession.exists(userWSRef)) {
+
+            if (log.isDebugEnabled()) {
+                log.debug(" -- Création du userWorkspace du user [" + userName + "] : " + userWSRef.value);
+            }
+            
+            uw = super.doCreateUserWorkspace(unrestrictedSession, userWSRef, principal, userName);
+
+        }
+        //
+		// Paramétrage affichage portail
+		// userWorkspace.setProperty("toutatice", "pageTemplate",
+		// "/templates/userWorkspace");
+        uw.setProperty("toutatice", "tabOrder", "100");
+        uw.setPropertyValue(DC_TITLE, "Mon espace");
+
+		uw = unrestrictedSession.saveDocument(uw);
+		
+		String workspaceId = null;
+		Serializable propertyValue = uw.getPropertyValue("webc:url");
+		if(propertyValue != null) {
+			workspaceId = propertyValue.toString();
+		}
+
+		// Initialisation Mes documents
+		DocumentModel mesDocs = unrestrictedSession.createDocumentModel(
+				uw.getPathAsString(), "documents", "Folder");
+		mesDocs.setPropertyValue(DC_TITLE, "Documents");
+		mesDocs.setPropertyValue(TTC_SHOW_IN_MENU, Boolean.TRUE);
+		unrestrictedSession.createDocument(mesDocs);
+		
+		if(workspaceId != null) {
+			// Initialisation Recherche
+			DocumentModel searchStaple = unrestrictedSession.createDocumentModel(
+					uw.getPathAsString(), "search-staple", "Staple");
+			searchStaple.setPropertyValue(DC_TITLE, "Recherche");
+			searchStaple.setPropertyValue(TTC_SHOW_IN_MENU, Boolean.TRUE);		
+			searchStaple.setPropertyValue(TTC_TEMPLATE, "/default/templates/workspace/search");		
+			searchStaple.setPropertyValue(TTC_WEBID, "workspace_"+workspaceId+"_search");		
+
+			unrestrictedSession.createDocument(searchStaple);
+		}
+		
+
+        return uw;
+    }
+
+    protected DocumentModel createUserWorkspacesRootSegment(CoreSession unrestrictedSession, PathRef rootRef) throws ClientException {
+
+        String parentPath = new Path(rootRef.toString()).removeLastSegments(1).toString();
+        String docName = new Path(rootRef.toString()).lastSegment();
+        DocumentModel doc = unrestrictedSession.createDocumentModel(parentPath, docName, getUserWorkspaceRootType());
+        doc.setProperty("dublincore", "title", docName);
+        doc.setProperty("dublincore", "description", "");
+        doc = unrestrictedSession.createDocument(doc);
+
+        super.setUserWorkspaceRootACL(doc);
+
+        return doc;
+    }
+
+
 }
