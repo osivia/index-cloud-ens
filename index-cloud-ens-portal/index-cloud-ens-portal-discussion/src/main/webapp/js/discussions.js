590c5ae921dfecb3b440b0beb215c38f1d685daf
ckrommenhoek <cedric.krommenhoek@gmail.com>
2020-01-14 18:06:39 +0100

    :art: Flexbox tables.

--- index-cloud-ens-portal/index-cloud-ens-portal-discussion/src/main/webapp/js/discussions.js
+++ index-cloud-ens-portal/index-cloud-ens-portal-discussion/src/main/webapp/js/discussions.js
@@ -28,7 +28,36 @@ $JQry(function() {
                 $modal.find("input[type=submit]").click();
             });
 
-          
+            $element.data("loaded", true);
+        }
+    });
+
+
+    $JQry(".discussions").each(function(index, element) {
+        var $element = $JQry(element);
+
+        if (!$element.data("loaded")) {
+           // Click on shadowbox
+            $element.find(".discussions-shadowbox").click(function (event) {
+                var $target = $JQry(event.target);
+                var $selectee = $target.closest(".portal-table-selectable-filter");
+                var $selectable = $selectee.closest(".portal-table-selectable");
+
+                if (event.ctrlKey) {
+                    $selectee.removeClass("ui-selected bg-primary border-primary");
+                } else {
+                    $selectable.find(".ui-selected").each(function (index, element) {
+                        var $element = $JQry(element);
+
+                        if (!$element.is($selectee)) {
+                            $element.removeClass("ui-selected bg-primary border-primary");
+                        }
+                    });
+                }
+
+                // Update toolbar
+                updateTableToolbar($target);
+            });
 
             $element.data("loaded", true);
         }
